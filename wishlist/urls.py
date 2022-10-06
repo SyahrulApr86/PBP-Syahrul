a8899470977ef8a9de6dcca7c8258d4aa9ac7fcc
SyahrulApr86 <apriansyah.syahrul@gmail.com>
2022-10-06 14:38:10 +0700

    create ajax (fail)

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -5,11 +5,13 @@ app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('ajax/', show_wishlist_ajax, name='show_wishlist_ajax'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
+    path('logout/', logout_user, name='logout'),path('ajax/submit/', create, name='tbl-form'),
+
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.http.response import JsonResponse
 
 # Create your views here.
 
@@ -24,6 +25,17 @@ def show_wishlist(request):
     }
 
     return render(request, "wishlist.html", context)
+
+@login_required(login_url='/wishlist/login/')
+def show_wishlist_ajax(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Syahrul Apriansyah',
+        'last_login': request.COOKIES['last_login'],
+    }
+
+    return render(request, "wishlist_ajax.html", context)
 
 def show_xml(request):
     data = BarangWishlist.objects.all()
@@ -76,3 +88,14 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('wishlist:login'))
     response.delete_cookie('last_login')
     return response
+
+
+@login_required(login_url='/wishlist/login/')
+def create(request):
+    if request.method == 'POST':
+        nama_barang = request.POST.get('nama_barang')
+        harga_barang = request.POST.get('harga_barang')
+        deskripsi = request.POST.get('deskripsi')
+        item = BarangWishlist(nama_barang=nama_barang, harga_barang=harga_barang, deskripsi=deskripsi)
+        item.save()
+        return JsonResponse({"instance": "Proyek Dibuat"}, status=200)
